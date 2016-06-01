0baeafcb04696c65194b9729a93c64d42d933891
Adam Bellinson <adambellinson@quickenloans.com>
2016-06-01 17:19:54 -0400

    config

--- config/connections.js
+++ config/connections.js
@@ -3,26 +3,9 @@ module.exports.connections = {
     module:   'sails-mysql',
     host:     'localhost',
     port:     3306,
-    user: 'lucidboard',
-    password: 'coeudoecudocudcoud',
+    user:     'lucidboard',
+    password: '***',
     database: 'lucidboard_dev',
     charset:  'utf8'
-  },
-  redis: {
-    adapter: 'sails-redis',
-    port: 6379,
-    host: 'localhost',
-    password: null,
-    database: null,
-    options: {
-      // low-level configuration
-      // (redis driver options)
-      // parser: 'hiredis',
-      // return_buffers: false,
-      // detect_buffers: false,
-      // socket_nodelay: true,
-      // no_ready_check: false,
-      // enable_offline_queue: true
-    }
   }
 };
